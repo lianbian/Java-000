41c14b44c1812a397b332e92cc074b724ab0f8ed
chengbin <php_er@126.com>
2020-10-19 23:41:39 +0800

    作业提交

---
+++ Hello.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class Hello extends ClassLoader{
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = new Hello().findClass("Hello");
+            Object obj = aClass.newInstance();
+            Method method = aClass.getMethod("hello");
+            method.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (java.lang.reflect.InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File file = new File(this.getClass().getResource("./Hello.xlass").getPath());
+        int length = (int)file.length();
+        byte[] bytes = new byte[length];
+        try {
+            new FileInputStream(file).read(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return super.findClass(name);
+        }
+        for (int i=0; i<bytes.length; ++i) {
+            bytes[i] = (byte)(255 - bytes[i]);
+        }
+
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+}
