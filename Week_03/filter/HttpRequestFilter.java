88b3618aa82466571685130cf11277124ae1462e
chengbin <php_er@126.com>
2020-11-06 00:15:02 +0800

    第三周作业

---
+++ Week_03/filter/HttpRequestFilter.java
@@ -0,0 +1,10 @@
+package Week_03.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public interface HttpRequestFilter {
+    
+    void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx);
+    
+}
